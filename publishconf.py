f3cec162891e9a0de44ab36fbbf3a297ab81cd56
Benjamin A. Laken <benlaken@gmail.com>
2016-11-28 10:59:22 +0100

    still modifying urls

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://benlaken.github.io'
+SITEURL = 'http://benlaken.github.io/blogsite'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
